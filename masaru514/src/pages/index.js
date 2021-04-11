5d11951534a5c8d5d9bc7323de8efc6cb719bcd7
masaru514 <415urasam@gmail.com>
2021-04-12 02:09:41 +0900

    update: blog styles, graphql

--- masaru514/src/pages/index.js
+++ masaru514/src/pages/index.js
@@ -1,6 +1,10 @@
 import * as React from 'react'
 import Layout from '../layout/default'
 import Head from '../components/Head'
+import { useStaticQuery, graphql } from 'gatsby'
+import styled from 'styled-components'
+import { format } from 'date-fns'
+import { utcToZonedTime } from 'date-fns-tz'
 
 // markup
 const IndexPage = () => {
@@ -8,10 +12,59 @@ const IndexPage = () => {
     <Layout>
       <Head title="HOME" />
       <main>
-        <div>Hello World</div>
+        <h1>masaru514 技術書</h1>
+        <Articles />
       </main>
     </Layout>
   )
 }
 
+const Articles = () => {
+  const { allContentfulMasaru514Blog } = useStaticQuery(query)
+  const articles = allContentfulMasaru514Blog.nodes
+
+  const Section = styled.section`
+    border: 1px solid #f8f8f8;
+    margin-bottom: 2rem;
+    border-radius: 5px;
+    padding: 2rem;
+  `
+
+  return articles.map((article) => {
+    const jstCreatedAt = utcToZonedTime(article.createdAt, 'Asia/Tokyo')
+    const createdAt = format(jstCreatedAt, 'yyyy/MM/dd HH:mm')
+
+    const jstUpdatedAt = utcToZonedTime(article.updatedAt, 'Asia/Tokyo')
+    const updatedAt = format(jstUpdatedAt, 'yyyy/MM/dd HH:mm')
+    return (
+      <Section key={article.title}>
+        <p>作成日：{createdAt}</p>
+        {createdAt !== updatedAt ? <p>更新日：{updatedAt}</p> : ''}
+        <h2>{article.title}</h2>
+        <div dangerouslySetInnerHTML={{ __html: article.body.childrenMarkdownRemark[0].html }} />
+      </Section>
+    )
+  })
+}
+
 export default IndexPage
+
+const query = graphql`
+  query {
+    allContentfulMasaru514Blog {
+      nodes {
+        slug
+        tags
+        title
+        updatedAt
+        createdAt
+        body {
+          childrenMarkdownRemark {
+            html
+          }
+          id
+        }
+      }
+    }
+  }
+`
